91a2b755093adaad634ed9faf56e8f5436fde616
Oliver Kötter <oliver@koetter.cc>
2013-09-27 15:33:00 +0200

    writes back data to form

--- src/CardSort.js
+++ src/CardSort.js
@@ -3,6 +3,7 @@
   "use strict";
 
   //Configuration
+  var GridId = "Q4";
   var leftStart = 10;
   var topStart = 10;
   var leftOffset = 3;
@@ -20,6 +21,20 @@
       left += leftOffset; //increase offsets for new card in same zone
       top += topOffset;
     });
+  }
+
+  function writeData(card, dropzone) {
+    var re = /(\d+)$/;
+    var cardNo = re.exec(card)[0];
+
+    if(dropzone === "startzone") {
+      console.log("selector:", ".card[id^='" + GridId + "_" + cardNo + "']");
+      $("input[id^='" + GridId + "_" + cardNo + "']").prop("checked", false);
+    }
+    else {
+      var dropzoneNo = re.exec(dropzone)[0];
+      $("#" + GridId + "_" + cardNo + "_" + dropzoneNo).prop("checked", true);
+    }
   }
 
   //init card positions
@@ -42,6 +57,7 @@
     drop: function(event, ui) {
       $(this).append(ui.draggable);
       alignCards(this);
+      writeData(ui.draggable[0].classList[1], this.parentElement.classList[1]); //write data of dropped card into form
     }
   });
 }());
